d5a58c0d5607b6d3342092c033186be92d7c2eb5
kangsix6@126.com <kangsix6@126.com>
2017-03-10 19:22:46 +0800

    搭建

---
+++ NetNews/NetNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetNews
+//
+//  Created by kyx on 17/3/10.
+//  Copyright © 2017年 kyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
